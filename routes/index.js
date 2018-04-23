c189de7f63863f9d01182a7221df38e9dd8f4f1f
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-04-23 15:26:25 +0200

    Done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const request = require("request");
+const parseString = require("xml2js").parseString;
+const RSS = require("rss");
 
-module.exports = (app) => {
+module.exports = () => {
 	const router = require("express").Router();
 
 	router.get("/", (req, res) => {
@@ -21,6 +23,10 @@ module.exports = (app) => {
 
 		const url = "https://business.instagram.com/v2/business/async/blogcard/?number_to_load=30&fully_curated=false&request_uri=https%3A%2F%2Fbusiness.instagram.com%2Fblog%2F%3F&filter_cms_id=670018146504888&dpr=1";
 
+		const feed = new RSS({
+			title: "Instagram business blog"
+		});
+
 		request.post(url, options, function (error, response, body) {
 			if(error){
 				res.code(500);
@@ -32,19 +38,40 @@ module.exports = (app) => {
 
 			body = JSON.parse(body);
 
-			res.json(body);
-
 			let posts = body.payload.cards;
 
 			posts = posts.map((post) => {
 				return post.__html;
 			}).map((post) => {
-				console.log(post.split("</div><div class=")[0]);
+				return new Promise((resolve, reject) => {
+					parseString(post, function (err, result) {
+						if(err){
+							reject(err);
+						}
 
-				//console.log(post);
+						resolve(result);
+					});
+				});
 			});
 
-			//console.log(JSON.stringify(posts, null, 4));
+			Promise.all(posts).then((posts) => {
+				posts.map((post) => {
+					return post.div.div[0].a;
+				}).map((post) => {
+					const postData = {
+						title: post[0].div[1].div[1]._,
+						description: post[0].div[1].div[2].p[1]._,
+						url: "https://business.instagram.com" + post[0].$.href,
+						date: post[0].div[1].div[0].div[0].abbr[0].$["data-utime"]*1000
+					};
+
+					return postData;
+				}).forEach((post) => {
+					feed.item(post);
+				});
+
+				res.send(feed.xml());
+			});
 		});
 	});
 
